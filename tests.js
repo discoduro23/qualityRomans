61501c887c2e4411fc606f1a09d9f911fa397b34
Daniel Sanchez <73238095+discoduro23@users.noreply.github.com>
2024-05-07 14:09:50 +0200

    jj

--- tests.js
+++ tests.js
@@ -1,46 +1,34 @@
-// Some messages are changed from the previous work, to match with the actual code
-test("TC-1: Valid Input (Single Digit)", function(assert) {
-  assert.propEqual(convertIntegerToRoman(1), {value: "I", message: "", result: true}, "TC-1");
+test("Equilateral", function (assert) {
+    assert.equal(getTriangleType(2,2,2), EQUILATERAL, "NORMAL");
+    assert.equal(getTriangleType(0.1,0.1,0.1), EQUILATERAL, "SMALL DECIMAL NUMBERS");
+    assert.equal(getTriangleType(99999,99999,99999), EQUILATERAL, "BIG INTEGERS");
 });
 
-test("TC-2: Valid Input (Multiple Digits)", function(assert) {
-  assert.propEqual(convertIntegerToRoman(3999), {value: "MMMCMXCIX", message: "", result: true}, "TC-2");
+test("Isosceles", function (assert) {
+    assert.equal(getTriangleType(2,2,3), ISOSCELES, "LAST ONE DIFFERENT");
+    assert.equal(getTriangleType(2,3,2), ISOSCELES, "MIDDLE ONE DIFFERENT");
+    assert.equal(getTriangleType(3,2,2), ISOSCELES, "FIRST ONE DIFFERENT");
 });
 
-test("TC-3: Negative Input (Not-Valid Number)", function(assert) {
-  assert.propEqual(convertIntegerToRoman(-1), {value: 0, message: "Please enter a valid integer", result: false}, "TC-3");
+test("Scalene", function (assert) {
+    assert.equal(getTriangleType(2,3,4), SCALENE, "LAST BIGGEST");
+    assert.equal(getTriangleType(2,4,3), SCALENE, "MIDDLE BIGGEST");
+    assert.equal(getTriangleType(4,3,2), SCALENE, "FIRST BIGGEST");
 });
 
-test("TC-4: Invalid Input (Number Less Than Range)", function(assert) {
-  assert.propEqual(convertIntegerToRoman(0), {value: 0, message: "Out of range (1-3999)", result: false}, "TC-4");
+test("Not a Triangle", function (assert) {
+    assert.equal(getTriangleType(1,3,4), NOT_A_TRIANGLE, "BIG IS LAST ONE");
+    assert.equal(getTriangleType(4,3,1), NOT_A_TRIANGLE, "BIG IS FIRST ONE");
+    assert.equal(getTriangleType(2,5,3), NOT_A_TRIANGLE, "BIG IS MIDDLE ONE");
+    assert.equal(getTriangleType(2,5,2.99999999), NOT_A_TRIANGLE, "BIG IS MIDDLE ONE WITH DECIMALS");
 });
 
-test("TC-5: Invalid Input (Number Greater Than Range)", function(assert) {
-  assert.propEqual(convertIntegerToRoman(4000), {value: 0, message: "Out of range (1-3999)", result: false}, "TC-5");
+test("Invalid Arguments", function (assert) {
+    assert.equal(getTriangleType(0,0,0), INVALID_ARGUMENTS , "ALL ZEROS");
+    assert.equal(getTriangleType(-1,1,1), INVALID_ARGUMENTS, "FIRST ONE INVALID");
+    assert.equal(getTriangleType(2,-5,3), INVALID_ARGUMENTS, "SECOND ONE INVALID");
+    assert.equal(getTriangleType(2,5,-2.99999999), INVALID_ARGUMENTS, "THIRD ONE INVALID");
+    assert.equal(getTriangleType("A",1,1), INVALID_ARGUMENTS, "FIRST ONE LETTER");
+    assert.equal(getTriangleType(2,"!",3), INVALID_ARGUMENTS, "SECOND ONE SPECIAL CHARACTER");
+    assert.equal(getTriangleType(2,5,"AA"), INVALID_ARGUMENTS, "THIRD ONE LETTER");
 });
-
-test("TC-6: Invalid Input (Non-Numeric String)", function(assert) {
-  assert.propEqual(convertIntegerToRoman("hello"), {value: 0, message: "Please enter a valid integer", result: false}, "TC-6");
-});
-
-test("TC-7: Valid Input (Simple Roman Numeral)", function(assert) {
-  assert.propEqual(convertRomanToInteger("X"), {value: 10, message: "", result: true}, "TC-7");
-});
-
-test("TC-8: Valid Input (Complex Roman Numeral):", function(assert) {
-  assert.propEqual(convertRomanToInteger("MMII"), {value: 2002, message: "", result: true}, "TC-8");
-});
-
-test("TC-9: Invalid Input (Empty String)", function(assert) {
-  assert.propEqual(convertRomanToInteger(""), {value: 0, message: "Please enter a valid roman", result: false}, "TC-9");
-});
-
-test("TC-10: Invalid Input (Invalid Characters)", function(assert) {
-  assert.propEqual(convertRomanToInteger("X11"), {value: 0, message: "Please enter a valid roman", result: false}, "TC-10");
-});
-
-test("TC-11: Invalid Input (Incorrect Symbol Repetition)", function(assert) {
-  assert.propEqual(convertRomanToInteger("IIII"), {value: 0, message: "Please enter a valid roman", result: false}, "TC-11");
-});
-
-
